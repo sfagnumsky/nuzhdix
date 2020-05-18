7712db62f162b449cae227a1dd873fd8436b1573
Sphagnum palustre <42124659+sfagnumsky@users.noreply.github.com>
2020-05-18 14:48:26 +0300

    Update i-love-cats.user.js

--- i-love-cats.user.js
+++ i-love-cats.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nuzhdixTheGreatKnopka
 // @namespace    nuzhdix
-// @version      2.2.8.0.1
+// @version      2.2.8.0.3
 // @description Opisanie. Opisal?
 // @author       dik&dok
 // @require http://raw.githubusercontent.com/rndtrash/nushdiki.js/master/nushdiki.js
@@ -14,10 +14,13 @@
 var knopqa = document.createElement('li');
 knopqa.title = "Нуждик!";
 var fatcock = document.createElement('span');
-fatcock.onclick = function () {document.getElementById('text_msg').value+="\n"+random_nushdik();}
+fatcock.onclick = function () {if (document.getElementById('text_msg').value=="") document.getElementById('text_msg').value+=random_nushdik(); else document.getElementById('text_msg').value+="\n"+random_nushdik();}
 fatcock.style.height = '24px';
 fatcock.style.position = 'relative';
 fatcock.style.background = 'url("http://live.staticflickr.com/65535/49908501406_51565e8549_o.png") left no-repeat';
 fatcock.style.cursor = 'pointer';
 document.getElementsByClassName('bbcode_forum')[0].append(knopqa);
+var razdelitel = document.createElement('li');
+razdelitel.class = 'separator';
+document.knopqa.before(razdelitel);
 knopqa.append(fatcock);
