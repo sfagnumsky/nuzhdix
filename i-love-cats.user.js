7fb8575c3bb08164d1e65b4aa33705e1e6e1f3ef
Sphagnum palustre <42124659+sfagnumsky@users.noreply.github.com>
2020-05-18 13:24:08 +0300

    Update i-love-cats.user.js

--- i-love-cats.user.js
+++ i-love-cats.user.js
@@ -6,11 +6,15 @@
 // @author       dik&dok
 // @match        *://ru-minecraft.ru/forum/showtopic-*
 // @grant        none
-// @downloadUrl http://raw.githubusercontent.com/sfagnumsky/nuzhdix/dungeonmaster/i-love-cats.user.js
-// @updateUrl http://raw.githubusercontent.com/sfagnumsky/nuzhdix/dungeonmaster/i-love-cats.user.js 
+// @downloadUrl http://github.com/sfagnumsky/nuzhdix/raw/dungeonmaster/i-love-cats.user.js
+// @updateUrl http://github.com/sfagnumsky/nuzhdix/raw/dungeonmaster/i-love-cats.user.js 
 // ==/UserScript==
 
 
 'use strict';
 import * as nuzhd from 'http://raw.githubusercontent.com/rndtrash/nushdiki.js/master/nushdiki.js';
 // Thanks ivan2020 ili kak tam tebya zovut for original nushdiki.js
+var knopqa = document.createElement('li');
+knopqa.title = "Нуждик!";
+var fatcock = document.createElement('span');
+fatcock.onclick = document.getElementById('text_msg').value+="\n"+nuzhd.random_nushdik();
