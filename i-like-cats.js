9de4dda2af667273cb3fe7212f75951ea39ea784
Sphagnum palustre <42124659+sfagnumsky@users.noreply.github.com>
2020-05-18 12:42:46 +0300

    da da net da
    ]s[qwplfllq

---
+++ i-like-cats.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         nuzhdixTheGreatKnopka
+// @namespace    nuzhdix
+// @version      2.2.8.0.1
+// @description Opisanie. Opisal?
+// @author       dik&dok
+// @match        *://ru-minecraft.ru/forum/showtopic-*
+// @grant        none
+// @downloadUrl 
+// @updateUrl   
+// ==/UserScript==
+
+
+'use strict';
+import * as nuzhd from 'http://raw.githubusercontent.com/rndtrash/nushdiki.js/master/nushdiki.js';
+// Thanks ivan2020 ili kak tam tebya zovut for original nushdiki.js
