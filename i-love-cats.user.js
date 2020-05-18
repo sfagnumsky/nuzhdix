44712e0e92fe20adb89c67b03d04f273d26768e2
Sphagnum palustre <42124659+sfagnumsky@users.noreply.github.com>
2020-05-18 17:37:52 +0300

    Update i-love-cats.user.js

--- i-love-cats.user.js
+++ i-love-cats.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              nuzhdixTheGreatKnopka
 // @namespace         nuzhdix
-// @version           2.2.8.0.4
+// @version           2.2.8.0.5
 // @description       Opisanie. Opisal?
 // @author            dik&dok
 // @require           http://raw.githubusercontent.com/rndtrash/nushdiki.js/master/nushdiki.js
@@ -24,3 +24,13 @@ var razdelitel = document.createElement('li');
 razdelitel.className = 'separator';
 knopqa.before(razdelitel);
 knopqa.append(fatcock);
+var mne = document.createElement('li');
+mne.title = "Мне повезет!";
+var povezet = document.createElement('span');
+povezet.onclick = function () {document.getElementById('text_msg').value+="\n"+random_nushdik();document.getElementsByClassName('b01')[0].click();}
+povezet.style.height = '24px';
+povezet.style.position = 'relative';
+povezet.style.background = 'url("http://live.staticflickr.com/65535/49909448202_52ef1f1038_o.png") left no-repeat';
+povezet.style.cursor = 'pointer';
+knopqa.after(mne);
+mne.append(povezet);
